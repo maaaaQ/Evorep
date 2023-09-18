d2b03483810b4bc09f27bfe49bcdfbb9bd5543a6
Yuri Makeev <makeevyuri7@gmail.com>
2023-09-18 19:31:47 +0300

    add first functions

---
+++ calc.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        print("Ошибка: деление на ноль")
+        return None
+    return a / b
